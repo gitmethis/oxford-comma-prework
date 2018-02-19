ef5120ed6ece32b8070cda2b4fb9856a6c550273
Chase <chaseit12@gmail.com>
2018-02-19 23:01:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,11 @@ def oxford_comma(array)
    
    if array.length == 1
       return array[0]
+   elsif array.length == 2
+      return array[0] + ' and ' + array[1]
    end
+   
+   
    
    
    array.each{ |word|
